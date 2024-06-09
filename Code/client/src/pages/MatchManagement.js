6c7222181ac9e227a1894965bc8935b48aa1a885
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-09 11:42:48 -0700

    commented out player/matches forms until auth is implemented

--- Code/client/src/pages/MatchManagement.js
+++ Code/client/src/pages/MatchManagement.js
@@ -308,7 +308,44 @@ function MatchManagement() {
                     </div>
                 ))}
             </div>
-            <form onSubmit={handleSubmit} className="bodycontent">
+
+
+            {isModalOpen && selectedMatch && (
+                <div className="modal">
+                    <div className="modal-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <h2 className="modal-title">Match Details</h2>
+                        <div className="modal-body">
+                            <div className={`team-column ${isTeamAWinner(selectedMatch) ? 'winner' : 'loser'}`}>
+                                <h3>Team A</h3>
+                                <p className={`score ${isTeamAWinner(selectedMatch) ? 'winning-score' : 'losing-score'}`}>
+                                    {selectedMatch.scores[0]}
+                                </p>
+                                <p>{selectedMatch.teams[0].map(id => getPlayerName(id)).join(', ')}</p>
+                            </div>
+                            <div className={`triangle ${isTeamAWinner(selectedMatch) ? 'triangle-left' : 'triangle-right'}`}></div>
+                            <div className={`team-column ${!isTeamAWinner(selectedMatch) ? 'winner' : 'loser'}`}>
+                                <h3>Team B</h3>
+                                <p className={`score ${!isTeamAWinner(selectedMatch) ? 'winning-score' : 'losing-score'}`}>
+                                    {selectedMatch.scores[1]}
+                                </p>
+                                <p>{selectedMatch.teams[1].map(id => getPlayerName(id)).join(', ')}</p>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <p><strong>Date:</strong> {formatDate(selectedMatch.date)} <strong> | Location:</strong> {selectedMatch.location}</p>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default MatchManagement;
+
+/*
+<form onSubmit={handleSubmit} className="bodycontent">
                 <div className="teams">
                     <div className="team">
                         <h3>Team A</h3>
@@ -382,38 +419,4 @@ function MatchManagement() {
                     <button type="submit">Add Match</button>
                 </div>
             </form>
-
-            {isModalOpen && selectedMatch && (
-                <div className="modal">
-                    <div className="modal-content">
-                        <span className="close" onClick={closeModal}>&times;</span>
-                        <h2 className="modal-title">Match Details</h2>
-                        <div className="modal-body">
-                            <div className={`team-column ${isTeamAWinner(selectedMatch) ? 'winner' : 'loser'}`}>
-                                <h3>Team A</h3>
-                                <p className={`score ${isTeamAWinner(selectedMatch) ? 'winning-score' : 'losing-score'}`}>
-                                    {selectedMatch.scores[0]}
-                                </p>
-                                <p>{selectedMatch.teams[0].map(id => getPlayerName(id)).join(', ')}</p>
-                            </div>
-                            <div className={`triangle ${isTeamAWinner(selectedMatch) ? 'triangle-left' : 'triangle-right'}`}></div>
-                            <div className={`team-column ${!isTeamAWinner(selectedMatch) ? 'winner' : 'loser'}`}>
-                                <h3>Team B</h3>
-                                <p className={`score ${!isTeamAWinner(selectedMatch) ? 'winning-score' : 'losing-score'}`}>
-                                    {selectedMatch.scores[1]}
-                                </p>
-                                <p>{selectedMatch.teams[1].map(id => getPlayerName(id)).join(', ')}</p>
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <p><strong>Date:</strong> {formatDate(selectedMatch.date)} <strong> | Location:</strong> {selectedMatch.location}</p>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
-}
-
-export default MatchManagement;
-
+            */
