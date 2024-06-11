80b0aa79e54dd5334af345ebb92065c9c9e505b2
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-10 22:00:36 -0700

    nav and footer on contacts

--- Code/client/src/pages/Contact.js
+++ Code/client/src/pages/Contact.js
@@ -1,12 +1,16 @@
 // src/pages/Contact.js
 import React from 'react';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
 import '../styles.css';
 
 function Contact() {
     return (
         <div className="contact-information">
+            <NavBar />
             <h1>Contact Information</h1>
             <p>Your contact information content goes here.</p>
+            <Footer />
         </div>
     );
 }
