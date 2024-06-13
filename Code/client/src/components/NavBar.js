234143f47472e3e72596d617eddf91d91b6afe84
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-12 23:27:22 -0700

    mobile navbar

--- Code/client/src/components/NavBar.js
+++ Code/client/src/components/NavBar.js
@@ -1,17 +1,17 @@
-// src/components/NavBar.js
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link,useLocation } from 'react-router-dom';
 import '../styles.css';
 
 function NavBar() {
     const location = useLocation();
+    const [menuOpen,setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
     return (
-        <header className="header">
-            <nav className="nav-left">
-                <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
-                <Link to="/combos" className={location.pathname === '/combos' ? 'active' : ''}>Combos</Link>
-            </nav>
+        <header className={`header ${menuOpen ? 'open' : ''}`}>
             <div className="title-container">
                 <img src="/images/VolleyVibe.png" alt="VolleyVibe Logo" className="logo" />
                 <div className="title">
@@ -19,10 +19,25 @@ function NavBar() {
                     <div className="vibe">Vibe!</div>
                 </div>
             </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                &#9776;
+            </div>
+            <nav className="nav-left">
+                <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
+                <Link to="/combos" className={location.pathname === '/combos' ? 'active' : ''}>Combos</Link>
+            </nav>
             <nav className="nav-right">
                 <Link to="/players" className={location.pathname === '/players' ? 'active' : ''}>Players</Link>
                 <Link to="/matches" className={location.pathname === '/matches' ? 'active' : ''}>Matches</Link>
             </nav>
+            {menuOpen && (
+                <nav className="mobile-nav">
+                    <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
+                    <Link to="/combos" className={location.pathname === '/combos' ? 'active' : ''}>Combos</Link>
+                    <Link to="/players" className={location.pathname === '/players' ? 'active' : ''}>Players</Link>
+                    <Link to="/matches" className={location.pathname === '/matches' ? 'active' : ''}>Matches</Link>
+                </nav>
+            )}
         </header>
     );
 }
