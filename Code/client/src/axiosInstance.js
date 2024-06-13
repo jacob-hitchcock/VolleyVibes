3fe90e03229175d3ae7ced4d8534e8e57502e4a3
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-13 04:11:41 -0700

    Revert axios base URL to use environment variable

--- Code/client/src/axiosInstance.js
+++ Code/client/src/axiosInstance.js
@@ -1,10 +1,10 @@
 // src/axiosInstance.js
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api'; //
+const baseURL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000/api';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://volleyvibes.onrender.com/api',
+    baseURL: baseURL,
     withCredentials: true, // Include credentials in requests
 });
 
