18e32e6fff39e9bedba4eab1e5b40a5bce136acc
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-14 23:47:42 -0700

    details

--- Code/client/src/components/MatchCard.js
+++ Code/client/src/components/MatchCard.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import '../styles.css';
+import Button from '@mui/material/Button';
 
 const MatchCard = ({ match,getWinners,getLosers,formatDate,openModal,handleEdit,handleDelete,isAdmin }) => (
     <div className="match-card">
         <div>Winners: {getWinners(match)}</div>
         <div>Losers: {getLosers(match)}</div>
         <div>Date: {formatDate(match.date)}</div>
-        <button style={{ marginRight: 5 }} onClick={() => openModal(match)}>Details</button>
+        <Button sx={{
+            backgroundColor: '#E7552B',
+            color: '#fff5d6',
+            border: 'none',
+            fontFamily: 'Coolvetica',
+            textTransform: 'none',
+            height: '25px',
+            borderRadius: '10px',
+            fontSize: '12px',
+            cursor: 'pointer',
+            margin: '10px 0',
+            '&:hover': {
+                backgroundColor: '#e03e00',
+            },
+        }}
+            onClick={() => openModal(match)}>Details</Button>
         {isAdmin && (
             <>
                 <button style={{ marginRight: 5 }} onClick={() => handleEdit(match)}>Edit</button>
