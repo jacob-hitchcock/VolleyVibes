961c8a8f1955a7a1d0982103d088a8710a7d9c4a
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-15 22:20:01 -0700

    testing

--- Code/client/src/components/StatCard.js
+++ Code/client/src/components/StatCard.js
@@ -5,7 +5,7 @@ import { styled } from '@mui/system';
 
 const StyledCard = styled(Card)({
     width: '100%',
-    margin: '10px', // Adjust margin to 10px for equal spacing on all sides
+    marginRight: '30px', // Adjust margin to 10px for equal spacing on all sides
     boxShadow: '3',
     border: '1px solid #e7552b',
     borderRadius: '5px',
