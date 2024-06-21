1401875db01fbcef2ec223128fabc5ae22c9dfce
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-21 14:01:22 -0700

    handle select/deselect all players

--- Code/client/src/components/PlayerCheckboxList.js
+++ Code/client/src/components/PlayerCheckboxList.js
@@ -1,21 +1,45 @@
 import React from 'react';
+import { Button } from '@mui/material';
 
-const PlayerCheckboxList = ({ players,selectedPlayers,handlePlayerSelect }) => (
-    <div className="players-list">
-        {players.map((player) => (
-            <div key={player._id} className="player-checkbox">
-                <label>
-                    <input
-                        type="checkbox"
-                        value={player._id}
-                        onChange={() => handlePlayerSelect(player._id)}
-                        checked={selectedPlayers.includes(player._id)}
-                    />
-                    {player.name}
-                </label>
+const PlayerCheckboxList = ({ players,selectedPlayers,handlePlayerSelect }) => {
+    const handleSelectAll = () => {
+        players.forEach(player => {
+            if(!selectedPlayers.includes(player._id)) {
+                handlePlayerSelect(player._id);
+            }
+        });
+    };
+
+    const handleDeselectAll = () => {
+        selectedPlayers.forEach(playerId => handlePlayerSelect(playerId));
+    };
+    return (
+        <div className='player-list-container'>
+            <div className="players-list">
+                {players.map((player) => (
+                    <div key={player._id} className="player-checkbox">
+                        <label>
+                            <input
+                                type="checkbox"
+                                value={player._id}
+                                onChange={() => handlePlayerSelect(player._id)}
+                                checked={selectedPlayers.includes(player._id)}
+                            />
+                            {player.name}
+                        </label>
+                    </div>
+                ))}
             </div>
-        ))}
-    </div>
-);
+            <div className="player-checkbox-actions">
+                <Button onClick={handleSelectAll} sx={{
+                    marginRight: '10px',color: '#e7552b',fontFamily: 'Coolvetica',
+                }}>Select All</Button>
+                <Button onClick={handleDeselectAll} sx={{
+                    color: '#e7552b',fontFamily: 'Coolvetica',
+                }}>Deselect All</Button>
+            </div>
+        </div>
+    );
+}
 
 export default PlayerCheckboxList;
