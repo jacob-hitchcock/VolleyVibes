35d682e3652951e371f63e9147e6aee08aecb466
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-23 20:20:24 -0700

    styled mui filter

--- Code/client/src/pages/MilestonesPage.js
+++ Code/client/src/pages/MilestonesPage.js
@@ -1,4 +1,5 @@
 import React,{ useState,useRef } from 'react';
+import { createTheme,ThemeProvider } from '@mui/material/styles';
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import useFetchData from '../hooks/useFetchData';
@@ -6,6 +7,10 @@ import useFilters from '../hooks/useFilters';
 import PlayerStats from '../components/PlayerStats';
 import Timeline from '../components/Timeline';
 import CircularProgress from '@mui/material/CircularProgress';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
 import '../styles.css';
 
 const MilestonesPage = () => {
@@ -39,13 +44,78 @@ const MilestonesPage = () => {
                     </div>
                 ) : (
                         <div>
-                            <label htmlFor="player-select">Filter by Player: </label>
-                            <select id="player-select" value={selectedPlayer} onChange={(e) => setSelectedPlayer(e.target.value)}>
-                                <option value="All">All</option>
-                                {players.map((player) => (
-                                    <option key={player._id} value={player.name}>{player.name}</option>
-                                ))}
-                            </select>
+                            <FormControl
+                                sx={{
+                                    display: 'block',
+                                    height: '56px', // Adjust height as needed
+                                    mb: 2, // Margin bottom to space it out from the header
+                                }}
+                                variant="outlined"
+                                margin="normal"
+                            >
+                                <InputLabel
+                                    id="player-select-label"
+                                    sx={{
+                                        fontFamily: 'coolvetica',
+                                        '&.Mui-focused': {
+                                            color: '#e7552b', // Label color when focused/active
+                                        },
+                                    }}
+                                >
+                                    Filter By Player
+              </InputLabel>
+                                <Select
+                                    labelId="player-select-label"
+                                    id="player-select"
+                                    value={selectedPlayer}
+                                    onChange={(e) => setSelectedPlayer(e.target.value)}
+                                    label="Filter by Player"
+                                    MenuProps={{ PaperProps: { sx: { maxHeight: 400,minWidth: 200 } } }}
+                                    sx={{
+                                        width: '200px',
+                                        fontFamily: 'coolvetica',
+                                        '& .MuiOutlinedInput-notchedOutline': {
+                                            borderColor: 'gray',
+                                        },
+                                        '&:hover .MuiOutlinedInput-notchedOutline': {
+                                            borderColor: 'black',
+                                        },
+                                        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                            borderColor: '#e7552b',
+                                        },
+                                        '&.Mui-selected': {
+                                            backgroundColor: '#ff7043', // Background colo
+                                            '&:hover': {
+                                                backgroundColor: '#ff7043', // Keep background color on hover when selected
+                                            },
+                                        },
+                                    }}
+                                >
+                                    <MenuItem value="All" sx={{
+                                        fontFamily: 'coolvetica',
+                                        '&.Mui-focused': {
+                                            backgroundColor: '#ff7043', // Background color when focused
+                                        },
+                                        '&.Mui-selected.Mui-focusVisible': {
+                                            backgroundColor: '#ff7043' //Set the highlight color to white on the first menu item when the menu opens
+                                        }
+                                    }}>All</MenuItem>
+                                    {players.map((player) => (
+                                        <MenuItem key={player._id} value={player.name} sx={{
+                                            fontFamily: 'coolvetica',
+                                            '&.Mui-focused': {
+                                                backgroundColor: '#ff7043', // Background color when focused
+                                            },
+                                            '&.Mui-selected': {
+                                                backgroundColor: '#ff7043', // Background colo
+                                                '&:hover': {
+                                                    backgroundColor: '#ff7043', // Keep background color on hover when selected
+                                                },
+                                            },
+                                        }}>{player.name}</MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
                             <ul>
                                 {players.map((player) => (
                                     <PlayerStats
