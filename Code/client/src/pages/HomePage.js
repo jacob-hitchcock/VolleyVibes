6af9453b5ba8a0d571149ffa48f0f9a9e7b53d55
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-25 13:31:36 -0700

    recent matches

--- Code/client/src/pages/HomePage.js
+++ Code/client/src/pages/HomePage.js
@@ -12,6 +12,10 @@ import SkeletonLeaderboard from '../components/SkeletonLeaderboard';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
 import { Link } from 'react-router-dom';
+import MatchGrid from '../components/MatchGrid';
+import SkeletonMatchGrid from '../components/SkeletonMatchGrid';
+import MatchDetailsModal from '../components/MatchDetailsModal';
+import { getPlayerName,formatDate,getWinners,getLosers,groupMatchesByDate,isTeamAWinner } from '../utils/utils';
 
 function HomePage() {
     const { matches,players,loading } = useFetchData();
@@ -29,6 +33,8 @@ function HomePage() {
     const { sortedPlayers,requestSort,getSortIndicator,sortConfig } = useSortedPlayers(aggregatedPlayerStats);
 
     const [openSnackbar,setOpenSnackbar] = useState(true);
+    const [isModalOpen,setIsModalOpen] = useState(false);
+    const [selectedMatch,setSelectedMatch] = useState(null);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -37,6 +43,27 @@ function HomePage() {
 
         return () => clearTimeout(timer);
     },[]);
+
+    // Find the most recent date from matches
+    const mostRecentDate = matches.length > 0 ? matches.reduce((latest,match) => {
+        const matchDate = new Date(match.date);
+        return matchDate > new Date(latest.date) ? match : latest;
+    }).date : null;
+
+    // Filter matches by the most recent date
+    const recentMatches = mostRecentDate ? matches.filter(match => match.date === mostRecentDate) : [];
+
+    const groupedMatches = groupMatchesByDate(recentMatches);
+
+    const openModal = (match) => {
+        setSelectedMatch(match);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedMatch(null);
+    };
 
     return (
         <div>
@@ -68,14 +95,40 @@ function HomePage() {
                             </div>
                         )}
                 </div>
+                <div className="recent-matches">
+                    <h2 className="match-title">Recent Matches</h2>
+                    {loading ? (
+                        <SkeletonMatchGrid />
+                    ) : (
+                            <MatchGrid
+                                groupedMatches={groupedMatches}
+                                getWinners={(match) => getWinners(match,(id) => getPlayerName(id,players))}
+                                getLosers={(match) => getLosers(match,(id) => getPlayerName(id,players))}
+                                formatDate={formatDate}
+                                openModal={openModal}
+                            />
+                        )}
+                    <div className="see-more">
+                        <Link to="/matches" className="see-more-link">See More</Link>
+                    </div>
+                </div>
             </main>
             <Footer />
-
             <Snackbar open={openSnackbar} autoHideDuration={10000} onClose={() => setOpenSnackbar(false)}>
                 <Alert onClose={() => setOpenSnackbar(false)} sx={{ width: '100%',backgroundColor: '#E7552B',color: '#fff5d6' }}>
                     Track new achievements on the <Link to="/milestones" style={{ color: '#fff5d6',textDecoration: 'underline' }}>milestones page</Link>!
                 </Alert>
             </Snackbar>
+            {isModalOpen && selectedMatch && (
+                <MatchDetailsModal
+                    className="match-management-modal"
+                    selectedMatch={selectedMatch}
+                    isTeamAWinner={isTeamAWinner}
+                    getPlayerName={(id) => getPlayerName(id,players)}
+                    formatDate={formatDate}
+                    closeModal={closeModal}
+                />
+            )}
         </div>
     );
 }
