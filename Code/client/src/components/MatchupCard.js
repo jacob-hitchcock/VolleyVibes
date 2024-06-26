d6ef14d8786c926ca3e9aa18bd766a7c8c794b3f
jacob-hitchcock <72181781+jacob-hitchcock@users.noreply.github.com>
2024-06-26 01:30:49 -0700

    predictions on card

--- Code/client/src/components/MatchupCard.js
+++ Code/client/src/components/MatchupCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../styles.css';
+import Divider from '@mui/material/Divider';
+
 
 const MatchupCard = ({
     matchup,
@@ -24,11 +26,12 @@ const MatchupCard = ({
         <div>
             <strong className="orange">Team B:</strong> {matchup.teamB.map(player => `${player.number}. ${player.name}`).join(', ')}
         </div>
+        <Divider />
         {prediction && (
-            <div className="prediction">
+            <div className="prediction" style={{ marginTop: '5px' }}>
                 <strong>Predicted Winner:</strong>
-                <p>Team A Probability: {prediction.teamAProbability.toFixed(2)}%</p>
-                <p>Team B Probability: {prediction.teamBProbability.toFixed(2)}%</p>
+                <p>Team A Probability: {(prediction.teamAProbability * 100).toFixed(2)}%</p>
+                <p>Team B Probability: {(prediction.teamBProbability * 100).toFixed(2)}%</p>
                 <p>Predicted Score - Team A: {prediction.teamAScore}, Team B: {prediction.teamBScore}</p>
             </div>
         )}
